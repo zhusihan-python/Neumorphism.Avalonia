87b0bb392bc7d1a70a43981f1e8415166e3589ef
allenz <1009698013@qq.com>
2024-12-28 17:07:42 +0800

    update Avalonia fix build error CS0535 as IResourceNode is NotClientImplementable

--- Avalonia.Themes.Neumorphism/Accents/SystemAccentColors.cs
+++ Avalonia.Themes.Neumorphism/Accents/SystemAccentColors.cs
@@ -6,7 +6,7 @@ using Avalonia.Styling;
 
 namespace Avalonia.Themes.Neumorphism.Accents
 {
-    internal class SystemAccentColors : IResourceProvider
+    internal class SystemAccentColors : ResourceProvider
     {
         public const string AccentKey = "SystemAccentColor";
         public const string AccentDark1Key = "SystemAccentColorDark1";
@@ -74,8 +74,8 @@ namespace Avalonia.Themes.Neumorphism.Accents
         private Color _systemAccentColorDark1, _systemAccentColorDark2, _systemAccentColorDark3;
         private Color _systemAccentColorLight1, _systemAccentColorLight2, _systemAccentColorLight3;
 
-        public bool HasResources => true;
-        public bool TryGetResource(object key, ThemeVariant theme, out object value)
+        public override bool HasResources => true;
+        public override bool TryGetResource(object key, ThemeVariant theme, out object value)
         {
             if (key is string strKey)
             {
